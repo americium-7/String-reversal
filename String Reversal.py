f6f84c8b5eec064e5bc5d9e790b43e177cc3a72d
americium-7 <adityamhetre009@gmail.com>
2025-11-09 21:17:48 +0530

    Update String Reversal.py

--- String Reversal.py
+++ String Reversal.py
@@ -1,13 +1,15 @@
 def reverse_string(input_str):
     """
-    Reverses the input string and returns the result.
+    Reverses the input string and returns the result.
+
     Args:
-        input_str (str): The string to be reversed
+        input_str (str): The string to be reversed
+
     Returns:
         str: The reversed string
     """
     return input_str[::-1]
 
-print(reverse_string("hello"))  # Output: "olleh"
+print(reverse_string("hii"))  # Output: "iih"
 print(reverse_string("Aditya"))  # Output: "aytidA"
 print(reverse_string("Mhetre"))  # Output: "ertehM"
